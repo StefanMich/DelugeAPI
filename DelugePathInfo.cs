46d0262c38e1593d912153d0f28082ea7bc33a53
deaddog <mikkelslsl@gmail.com>
2014-10-14 19:48:56 +0200

    Created a class for handling paths to deluge executables.

---
+++ DelugePathInfo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace DelugeAPI
+{
+    public static class DelugePathInfo
+    {
+        private static bool isSet;
+        private static string directory;
+
+        public static bool SetPath(string directory)
+        {
+            if (File.Exists(Path.Combine(directory, "deluge.exe")))
+            {
+                DelugePathInfo.directory = directory;
+                return true;
+            }
+            return false;
+        }
+
+        public static string Directory
+        {
+            get
+            {
+                if (directory == null)
+                    throw new NullReferenceException("No Deluge directory defined.");
+                return directory;
+            }
+        }
+
+        public static string DelugeConsoleExe
+        {
+            get { return Path.Combine(Directory, "deluge-console.exe"); }
+        }
+        public static string DelugedExe
+        {
+            get { return Path.Combine(Directory, "deluged.exe"); }
+        }
+    }
+}
