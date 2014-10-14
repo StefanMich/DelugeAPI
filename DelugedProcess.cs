efe7e5ce909f5430a7f0518017a46386b3cbe724
deaddog <mikkelslsl@gmail.com>
2014-10-14 19:49:49 +0200

    Created wrapper class for the deluged process.

---
+++ DelugedProcess.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+
+namespace DelugeAPI
+{
+    public class DelugedProcess
+    {
+        private Process process;
+
+
+        private static ProcessStartInfo getProcessInfo()
+        {
+            return new ProcessStartInfo(DelugePathInfo.DelugedExe);
+        }
+
+        private DelugedProcess()
+        {
+            this.process = null;
+        }
+
+        public static DelugedProcess Create()
+        {
+            return new DelugedProcess();
+        }
+
+        public void Start()
+        {
+            if (!Running)
+                this.process = Process.Start(getProcessInfo());
+        }
+        public void Stop()
+        {
+            if (!Running)
+                return;
+
+            process.Kill();
+            while (!process.HasExited) { }
+            process = null;
+        }
+
+        public bool Running
+        {
+            get { return process != null; }
+        }
+    }
+}
