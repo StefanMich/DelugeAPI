d31b877f1476ac87f6cdd78bd792f47a54111f2f
deaddog <mikkelslsl@gmail.com>
2014-10-17 20:30:23 +0200

    Added documentation to the DelugePathInfo class.

--- DelugePathInfo.cs
+++ DelugePathInfo.cs
@@ -3,11 +3,21 @@ using System.IO;
 
 namespace DelugeAPI
 {
+    /// <summary>
+    /// Provides methods for retrieving path to deluge executables.
+    /// The <see cref="SetPath"/> method must be called before any of the other methods.
+    /// </summary>
     public static class DelugePathInfo
     {
         private static bool isSet;
         private static string directory;
 
+        /// <summary>
+        /// Sets the path of the directory where the deluge executables are located.
+        /// This must be done before accessing any of the other properties or methods in this class.
+        /// </summary>
+        /// <param name="directory">The directory where the deluge executables are located.</param>
+        /// <returns><c>true</c> if the path was successfully set (if the required executables were found); otherwise <c>false</c>.</returns>
         public static bool SetPath(string directory)
         {
             if (File.Exists(Path.Combine(directory, "deluge.exe")) &&
@@ -20,6 +30,9 @@ namespace DelugeAPI
             return false;
         }
 
+        /// <summary>
+        /// Gets the path to the directory containing the deluged executables.
+        /// </summary>
         public static string Directory
         {
             get
@@ -30,10 +43,16 @@ namespace DelugeAPI
             }
         }
 
+        /// <summary>
+        /// Gets the path to the deluge-console executable.
+        /// </summary>
         public static string DelugeConsoleExe
         {
             get { return Path.Combine(Directory, "deluge-console.exe"); }
         }
+        /// <summary>
+        /// Gets the path to the deluged executable.
+        /// </summary>
         public static string DelugedExe
         {
             get { return Path.Combine(Directory, "deluged.exe"); }
