9ee99cf7cf6ee16a95fb7d7c00599acee3facf15
deaddog <mikkelslsl@gmail.com>
2014-10-17 15:46:39 +0200

    Added field, properties and a constructor to the TorrentInfo class and a nested FileInfo class.

--- TorrentInfo.cs
+++ TorrentInfo.cs
@@ -1,12 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DelugeAPI
 {
     public class TorrentInfo
     {
+        private string name;
+        private string id;
+        private long size;
+        private double progress;
+
+        private FileInfo[] files;
+
+        internal TorrentInfo(string name, string id, long size, double progress, IEnumerable<FileInfo> files)
+        {
+            this.name = name;
+            this.id = id;
+            this.size = size;
+            this.progress = progress;
+
+            this.files = files.ToArray();
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+        public string ID
+        {
+            get { return id; }
+        }
+        public long Size
+        {
+            get { return size; }
+        }
+        public double Progress
+        {
+            get { return progress; }
+        }
+
+        public class FileInfo
+        {
+            private string path;
+            private long size;
+            private double progress;
+
+            internal FileInfo(string path, long size, double progress)
+            {
+                this.path = path;
+                this.size = size;
+                this.progress = progress;
+            }
+
+            public string Path
+            {
+                get { return path; }
+            }
+            public long Size
+            {
+                get { return size; }
+            }
+            public double Progress
+            {
+                get { return progress; }
+            }
+        }
     }
 }
