8eb5917620b00a58b10ea3644026bbd16564f5ef
deaddog <mikkelslsl@gmail.com>
2014-10-17 16:25:24 +0200

    Added documentation for the DelugedProcess class.

--- DelugedProcess.cs
+++ DelugedProcess.cs
@@ -2,10 +2,12 @@
 
 namespace DelugeAPI
 {
+    /// <summary>
+    /// Exposes methods for starting/stopping a deluged server locally.
+    /// </summary>
     public class DelugedProcess
     {
         private Process process;
-
 
         private static ProcessStartInfo getProcessInfo()
         {
@@ -17,16 +19,27 @@ namespace DelugeAPI
             this.process = null;
         }
 
-        public static DelugedProcess Create()
+
+        private static DelugedProcess server = new DelugedProcess();
+        /// <summary>
+        /// Gets the local deluged-server (will not register a process that is already running).
+        /// </summary>
+        public static DelugedProcess Server
         {
-            return new DelugedProcess();
+            get { return server; }
         }
 
+        /// <summary>
+        /// Starts the server-instance referenced by this <see cref="DelugedProcess"/>.
+        /// </summary>
         public void Start()
         {
             if (!Running)
                 this.process = Process.Start(getProcessInfo());
         }
+        /// <summary>
+        /// Stops the server-instance referenced by this <see cref="DelugedProcess"/>.
+        /// </summary>
         public void Stop()
         {
             if (!Running)
@@ -37,6 +50,9 @@ namespace DelugeAPI
             process = null;
         }
 
+        /// <summary>
+        /// Gets a value indicating whether this <see cref="DelugedProcess"/> is running.
+        /// </summary>
         public bool Running
         {
             get { return process != null; }
